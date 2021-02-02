1ca9f72bb37499c16c6a7c9050dd856e5a5b83d0
Ken Chong <thienpow@gmail.com>
2021-02-02 17:10:29 +0800

    println

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 async fn main() {
     pretty_env_logger::init();
 
+    println!("esm-admin is running on port 3030");
     warp::serve(warp::fs::dir("www"))
         .run(([0, 0, 0, 0], 3030))
         .await;
